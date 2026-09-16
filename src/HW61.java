57e47ff5b1e60ee600fe981965d852fd5e4e18ac
Alexandra <krupskaya.aliaksandra@gmail.com>
2026-09-16 16:45:13 +0300

    QAT-36 - HW6.1 from sasha krupskaya

--- src/HW61.java
+++ src/HW61.java
@@ -10,4 +10,26 @@ public class HW61 {
         }
         System.out.println(summ);
     }
+    public static void max() {
+        int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
+        int max = Integer.MIN_VALUE;
+        for (int row = 0; row < array.length; row++) {
+            for (int col = 0; col < array[row].length; col++) {
+                if (max < array[row][col]) {
+                    max = array[row][col];
+                }
+            }
+        }
+        System.out.println(max);
+    }
+    public static void countElements() {
+        int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
+        int counter = 0;
+        for (int row = 0; row < array.length; row++) {
+            for (int col = 0; col < array[row].length; col++) {
+                counter = counter + 1;
+            }
+        }
+        System.out.println(counter);
+    }
 }
