3e95bd069bd1801b76643664d587be044b88dcb6
Alexandra <krupskaya.aliaksandra@gmail.com>
2026-09-17 13:57:00 +0300

    I have made the update base on the suggestions

--- src/HW61.java
+++ src/HW61.java
@@ -26,9 +26,7 @@ public class HW61 {
         int[][] array = {{1, 2, 3, 4, 5}, {6, 7, 8, 9}, {-1, -2, -3, -4}, {-5, -6}};
         int counter = 0;
         for (int row = 0; row < array.length; row++) {
-            for (int col = 0; col < array[row].length; col++) {
-                counter = counter + 1;
-            }
+            counter += array[row].length;
         }
         System.out.println(counter);
     }
