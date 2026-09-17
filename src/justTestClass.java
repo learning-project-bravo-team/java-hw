9f256002d6eba5c9074e7257d0aae6900d4625d9
Alexandra <krupskaya.aliaksandra@gmail.com>
2026-09-17 13:28:34 +0300

    deleted the file

--- src/justTestClass.java
+++ src/justTestClass.java
@@ -2,10 +2,4 @@ static void main(){
     System.out.println("Test");
 
     //HW61.summ();//print the summa of multi array
-    SKrupHW62.displayAllO();//print all 'о' Cyrillic one
-    System.out.println(" ");
-    SKrupHW62.countE();
-    SKrupHW62.subStrIndex();
-    System.out.println(" ");
-    SKrupHW62.countStringsWithotE();
 }
