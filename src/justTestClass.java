838b0bbda77a9e9e086c1991d7fa604295e0ea79
Alexandra <krupskaya.aliaksandra@gmail.com>
2026-09-17 12:02:31 +0300

    QAT-36 - HW6.1 from sasha krupskaya

--- src/justTestClass.java
+++ src/justTestClass.java
@@ -1,5 +1,11 @@
 static void main(){
     System.out.println("Test");
 
-    HW61.summ();//print the summa of multi array
+    //HW61.summ();//print the summa of multi array
+    SKrupHW62.displayAllO();//print all 'о' Cyrillic one
+    System.out.println(" ");
+    SKrupHW62.countE();
+    SKrupHW62.subStrIndex();
+    System.out.println(" ");
+    SKrupHW62.countStringsWithotE();
 }
