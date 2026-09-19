e9993e42bde859e0222577982d103caca7d19fd2
YanaAndreeva <yanaefimovani@gmail.com>
2026-09-19 20:16:35 +0300

    QAT-49 Homework from lecture 7, task 1

---
+++ src/yanaandreevaHw7.java
@@ -0,0 +1,30 @@
+public class yanaandreevaHw7 {
+
+    // Сложение двух чисел
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    // Вычитание двух чисел
+    static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Умножение двух чисел
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Деление двух чисел
+    static int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(add(10, 5));
+        System.out.println(subtract(10, 5));
+        System.out.println(multiply(10, 5));
+        System.out.println(divide(10, 5));
+    }
+}
