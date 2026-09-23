c6a0c5ca18453b899962edc22f86cebe28d0f8a3
ilyasayapin <kite@nxt.ru>
2026-09-23 23:21:13 +0300

    QAT-60: add IlyaHW8.1 solution

---
+++ src/ilya/ilya.HW8/Main.java
@@ -0,0 +1,37 @@
+package ilya.ilya.HW8;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        /**
+         * Задача №8.1.1
+         *  Create a public class called Cube without a constructor which gets one single private integer variable Side,
+         *  a getter GetSide() and a setter SetSide(int num) method for this property.
+         *  Initialise the side to 0
+         *  */
+        Cube cube = new Cube();
+        cube.setSide(0);
+
+        /**
+         * Задача №8.1.2
+         *  Write a class Block that creates a block (Duh..)
+         *  The constructor should take an array as an argument, this will contain 3 integers of the form [width, length, height]
+         *  */
+        int[] array = {1, 2, 3};
+        Block block = new Block(array);
+        block.getVolume();
+        block.getSurfaceArea();
+
+        /**
+         * Задача №8.1.3
+         *  Create a function that returns the name of the winner in a fight between two fighters.
+         *  Each fighter takes turns attacking the other and whoever kills the other first is victorious.
+         *  Death is defined as having health <= 0.         *
+         * Each fighter will be a Fighter object/instance
+         *  */
+        Fighter firstFighter = new Fighter("Lew", 10, 2);
+        Fighter secondFighter = new Fighter("Harry", 5, 4);
+        Fighter.theWinner(firstFighter, secondFighter, "Lew");
+    }
+}
